fa35a26e2202882044530906c77f62eb802b3c5f
CMS <cmsdossantos@gmail.com>
2022-02-03 15:13:41 +0100

    added Form addition to main state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
@@ -7,40 +7,46 @@ import Calendar from "./components/Calendar";
 import Day from "./components/Day";
 
 import generateRandomAppointments from "./utils";
+// import AppLogic from './components/AppLogic';
 
 const appointments = generateRandomAppointments(7);
 
-const App = () => (// note dat het een functional component is. (-,-) 
-  <Router>
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/calendar">Calendar view</Link>
-          </li>
-          <li>
-            <Link to="/day">Day view</Link>
-          </li>
-        </ul>
-      </nav>
-      <main>
-        <Switch>
-          <Route path="/calendar">
-            <Calendar appointments={appointments} />
-          </Route>
-          <Route path="/day">
-            <Day appointments={appointments.filter(app => app.day === 1)} />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </main>
-    </div>
-  </Router>
-);
+const App = () => { // note dat het een functional component is. (-,-) 
+  const [state, setState] = useState({staff: [], });
+  
+
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/calendar">Calendar view</Link>
+            </li>
+            <li>
+              <Link to="/day">Day view</Link>
+            </li>
+          </ul>
+        </nav>
+        <main>
+          <Switch>
+            <Route path="/calendar">
+              <Calendar appointments={appointments} />
+            </Route>
+            <Route path="/day">
+              <Day appointments={appointments.filter(app => app.day === 1)} />
+            </Route>
+            <Route path="/">
+              <Home appointments={appointments} state={state} setState={setState} />
+            </Route>
+          </Switch>
+        </main>
+      </div>
+    </Router>
+  )
+};
 export default App;
 
--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,15 @@
 import React from "react";
 import "./App.css";
+import "./Home.css";
+import Form from "./components/Form";
+import SideSection from "./components/Sidesection";
 
-export default () => {
-    return <div>
-        home
-    </div>
+export default (props) => { 
+
+    return (<div style={{display: "flex"}} >
+        <Form state={props.state} setState={props.setState} />
+        <SideSection state={props.state} />
+        <span></span>
+    </div>)
 
 }
